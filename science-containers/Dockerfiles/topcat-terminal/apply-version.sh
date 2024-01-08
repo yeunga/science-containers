20f725f8358aa8cb9e21ce8f9505c7d4e788c10d
yeunga <Alinga.Yeung@nrc-cnrc.gc.ca>
2024-01-08 09:35:48 -0800

    CADC-12910 Moved containers from science-containers-old to here. Initial re-organization of containers.

---
+++ science-containers/Dockerfiles/topcat-terminal/apply-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+topcatimage=images.canfar.net/skaha/topcat-terminal
+
+# Extract topcat version from image.
+VERSION=`docker run ${topcatimage}:latest topcat -version | grep -i TOPCAT.version | sed 's/.*ersion //'`
+
+# Prepare tags: topcat version and build version (timestamp).
+TAGS="$VERSION $(date -u +'%Y%m%dT%H%M%S')"
+echo "tags: $TAGS"
+
+# Tag image.
+for t in $TAGS; do
+   docker image tag ${topcatimage}:latest ${topcatimage}:$t
+done
+unset TAGS
+unset VERSION
+docker image list ${topcatimage}
